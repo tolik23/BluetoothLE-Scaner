c5fa824211b9e227a521f9fd504b9a8bb47d6f4c
tolik23 <tolik231081991@mail.ru>
2017-06-21 23:22:08 +0300

    ble scan

--- app/src/main/java/com/liutorovichgmail/anatoliy/ble/SampleGattAttributes.java
+++ app/src/main/java/com/liutorovichgmail/anatoliy/ble/SampleGattAttributes.java
@@ -7,6 +7,7 @@ public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
     public static byte[] data = {1,2,3,4,5};
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
     static {
         // Sample Services.
